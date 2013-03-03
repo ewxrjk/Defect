f4106a1097af4900b7f0fa31001d21c9bab223df
Richard Kettlewell <rjk@terraraq.org.uk>
2013-03-03 15:10:08 +0000

    PrefixTable: lookup data structure for GIF encoder

---
+++ Defect/PrefixTable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defect
+{
+  /// <summary>
+  /// An efficiently searchable table of up to 4096 prefixes
+  /// </summary>
+  public class PrefixTable
+  {
+    // TODO this implementation is quite allocation-heavy; we should be able to do with a single large array.
+
+    /// <summary>
+    /// A node represents a single prefix
+    /// </summary>
+    private class Node
+    {
+      /// <summary>
+      /// Pointers to all prefixes that extend this node
+      /// </summary>
+      public Node[] Next = new Node[256];
+
+      /// <summary>
+      /// The code for this node
+      /// </summary>
+      public int Code;
+    };
+
+    /// <summary>
+    /// All known nodes except <code>root</code>
+    /// </summary>
+    private Node[] allNodes = new Node[4096];
+
+    /// <summary>
+    /// The node that corresponds to an empty byte sequence
+    /// </summary>
+    private Node root = new Node()
+    {
+      Code = -1
+    };
+
+    /// <summary>
+    /// Find the longest prefix that matches part of a byte array
+    /// </summary>
+    /// <param name="bytes">Byte array to match against</param>
+    /// <param name="pos">Position in byte array to match against</param>
+    /// <param name="length">The length of the matched prefix</param>
+    /// <returns>The code for the longest matching prefix, or -1 if there isn't one</returns>
+    public int Find(byte[] bytes, int pos, out int length)
+    {
+      Node node = root;
+      length = 0;
+      while (pos < bytes.Length && node.Next[bytes[pos]] != null) {
+        node = node.Next[bytes[pos]];
+        ++pos;
+        ++length;
+      }
+      return node.Code;
+    }
+
+    /// <summary>
+    /// Add a new prefix
+    /// </summary>
+    /// <param name="newCode">The new code prefix's code</param>
+    /// <param name="oldCode">The prefix to extend, or -1 to add a single-byte prefix</param>
+    /// <param name="extra">The additional byte</param>
+    public void Add(int newCode, int oldCode, byte extra)
+    {
+      Node oldNode = (oldCode >= 0 ? allNodes[oldCode] : root);
+      Node newNode = new Node() { Code = newCode };
+      allNodes[newCode] = newNode;
+      oldNode.Next[extra] = newNode;
+    }
+
+  }
+}
---
+++ Tests/PrefixTableTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Defect;
+
+namespace Tests
+{
+  [TestClass]
+  public class PrefixTableTests
+  {
+    [TestMethod]
+    public void EmptyPrefixTableTest()
+    {
+      PrefixTable pt = new PrefixTable();
+      int length;
+      Assert.AreEqual(-1, pt.Find(new byte[0], 0, out length));
+      Assert.AreEqual(0, length);
+      Assert.AreEqual(-1, pt.Find(new byte[1] { 0 }, 0, out length));
+      Assert.AreEqual(0, length);
+      Assert.AreEqual(-1, pt.Find(new byte[1] { 1 }, 0, out length));
+      Assert.AreEqual(0, length);
+    }
+
+    [TestMethod]
+    public void OneBytePrefixTableTest()
+    {
+      PrefixTable pt = new PrefixTable();
+      int length;
+      for (int n = 0; n < 16; ++n) {
+        pt.Add(n, -1, (byte)n);
+      }
+      for (int n = 0; n < 16; ++n) {
+        Assert.AreEqual(n, pt.Find(new byte[1] { (byte)n }, 0, out length), "checking code");
+        Assert.AreEqual(1, length, "checking length");
+      }
+      Assert.AreEqual(-1, pt.Find(new byte[1] { 17 }, 0, out length), "checking code not found");
+      Assert.AreEqual(0, length, "checking length not found");
+    }
+
+    [TestMethod]
+    public void ShortPrefixTableTest()
+    {
+      PrefixTable pt = new PrefixTable();
+      int length;
+      pt.Add(0, -1, 0);
+      pt.Add(1, 0, 1);
+      pt.Add(2, 1, 2);
+      pt.Add(3, 2, 3);
+      Assert.AreEqual(0, pt.Find(new byte[2] { 0, 99 }, 0, out length));
+      Assert.AreEqual(1, length);
+      Assert.AreEqual(1, pt.Find(new byte[3] { 0, 1, 99 }, 0, out length));
+      Assert.AreEqual(2, length);
+      Assert.AreEqual(2, pt.Find(new byte[4] { 0, 1, 2, 99 }, 0, out length));
+      Assert.AreEqual(3, length);
+    }
+
+    [TestMethod]
+    public void DivergentPrefixTableTest()
+    {
+      PrefixTable pt = new PrefixTable();
+      int length;
+      pt.Add(0, -1, 0);
+      pt.Add(1, 0, 1);
+      pt.Add(2, 0, 2);
+      pt.Add(3, 0, 3);
+      Assert.AreEqual(0, pt.Find(new byte[2] { 0, 99 }, 0, out length));
+      Assert.AreEqual(1, length);
+      Assert.AreEqual(1, pt.Find(new byte[3] { 0, 1, 99 }, 0, out length));
+      Assert.AreEqual(2, length);
+      Assert.AreEqual(2, pt.Find(new byte[3] { 0, 2, 99 }, 0, out length));
+      Assert.AreEqual(2, length);
+      Assert.AreEqual(3, pt.Find(new byte[3] { 0, 3, 99 }, 0, out length));
+      Assert.AreEqual(2, length);
+    }
+
+    public void OffsetPrefixTableTest()
+    {
+      PrefixTable pt = new PrefixTable();
+      int length;
+      pt.Add(0, -1, 0);
+      pt.Add(1, 0, 1);
+      pt.Add(2, 0, 2);
+      pt.Add(3, 0, 3);
+      Assert.AreEqual(1, pt.Find(new byte[7] { 0, 1, 0, 2, 0, 3, 99 }, 0, out length));
+      Assert.AreEqual(2, length);
+      Assert.AreEqual(2, pt.Find(new byte[7] { 0, 1, 0, 2, 0, 3, 99 }, 2, out length));
+      Assert.AreEqual(2, length);
+      Assert.AreEqual(3, pt.Find(new byte[7] { 0, 1, 0, 2, 0, 3, 99 }, 4, out length));
+      Assert.AreEqual(2, length);
+    }
+  }
+}
