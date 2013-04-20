491bea2b366ad787c523c4b228858716d6dd4e8b
Richard Kettlewell <rjk@terraraq.org.uk>
2013-04-20 16:48:14 +0100

    SSE for cyclic_vn_64.
    Not as much faster as I'd hoped.

--- native/cyclicvn64.asm
+++ native/cyclicvn64.asm
@@ -1,109 +1,207 @@
 ; See http://msdn.microsoft.com/en-us/library/ms235286.aspx for ABI
 ;
-; Registers:
-;   rax   return
-;   rbx   preserve
-;   rcx   first integer arg
-;   rdx   second integer arg
-;   rsi   preserve
-;   rdi   preserve
+;   reg   ABI                        usage here
+;   rax   return                     transient misce
+;   rbx   preserve                   transient old cell value
+;   rcx   first integer arg          change counter
+;   rdx   second integer arg         transient loop counter
+;   rsi   preserve                   'from' pointer
+;   rdi   preserve                   'to' pointer
 ;   rsp
 ;   rbp   preserve
-;   r8    third integer arg
-;   r9    fourth integer arg
-;   r10   smash
-;   r11   smash
+;   r8    third integer arg          width
+;   r9    fourth integer arg         states
+;   r10   smash                      cell counter
+;   r11   smash                      -width
 ;   r12   preserve
 ;   r13   preserve
 ;   r14   preserve
 ;   r15   preserve
-;   xmm0-5   smash
-;   xmm6-16  preserve
-
+;   xmm0  smash                      {cell + 1 (mod states)} x 16
+;   xmm1  smash                      {01} x 16
+;   xmm2  smash                      {states} x 16
+;   xmm3  smash                      left; overflow mask
+;   xmm4  smash                      right
+;   xmm5  smash                      down
+;   xmm6  preserve                   up
+;   xmm7  preserve                   {FF} x 16
+;   xmm8  preserve                   stashed cell values
+;   xmm9  preserve                   overflow mask
+;   xmm10+ preserve
+;
 ; Of the args we only preserve r9.
 	
         .code
+
 ; int cyclic_vn_64(byte *from [rcx], byte *to [rdx],
 ;                  int width [r8], int states [r9]);
 ; Returns change count
 ; NB we don't handle the first or last row.
 cyclic_vn_64:
+        push rdi
         push rsi
         push rbx
+        sub rsp,16*3
+        movdqa [rsp],xmm6
+        movdqa [rsp+16],xmm7
+        movdqa [rsp+32],xmm8
+        mov rsi,rcx            ; rsi = source
+        mov rdi,rdx            ; rdi = destination
         mov r10,r8
         mov r11,r8
         sub r10,2              ; r10 = cells left
         neg r11                ; r11 = -width
-        mov rsi,r8             ; rsi = change count
+        xor rcx,rcx            ; rcx = change count
 ; first cell
-        mov al,[rcx]
+        mov al,[rsi]
         mov bl,al
         inc al
         cmp al,r9b
         jb first_nomod
         xor rax,rax
 first_nomod:
-        cmp al,[rcx+r8-1]      ; left (wrapped)
+        cmp al,[rsi+r8-1]      ; left (wrapped)
         je first_store
-        cmp al,[rcx+1]         ; right
+        cmp al,[rsi+1]         ; right
         je first_store
-        cmp al,[rcx+r8]        ; down
+        cmp al,[rsi+r8]        ; down
         je first_store
-        cmp al,[rcx+r11]       ; up
+        cmp al,[rsi+r11]       ; up
         je first_store
         mov al,bl              ; retrieve original
-        dec rsi
+        dec rcx
 first_store:
-        mov [rdx],al
+        mov [rdi],al
         inc rcx
-        inc rdx
-; all but first and last cells
+        inc rsi
+        inc rdi
+; process 16-byte "paragraphs"
+; xmm1 = 01 in all byte positions
+        pxor xmm0,xmm0
+        mov rax,1
+        movd xmm1,rax
+        pshufb xmm1,xmm0
+; xmm2 = <states> in all byte positions
+        movd xmm2,r9
+        pshufb xmm2,xmm0
+; xmm7 = ff in all byte positions
+        mov rax,0FFh
+        movd xmm7,rax
+        pshufb xmm7,xmm0
+; figure out how much work to do with SSE2
+; (1) the work happens in 16-byte chunks
+; (2) wrapping must be avoided in the last few bytes
+; The former is easy. For the latter we require that rsi+1+15 is before the
+; end of the cell row.  That means we cannot handle the last 16 cells here.
+; We already special-case the last one so that just means we need to drop
+; 15 from the count.
+        mov rdx,r10
+        sub rdx,15                ; can't do the last 15 bytes
+        jbe trailer
+        shr rdx,4                 ; scale down to dqwords
+        je trailer
+        mov rax,rdx
+        shl rax,4                 ; scale back up to bytes
+        sub r10,rax               ; length of trailer
         align 16
 mainloop:
-        mov al,[rcx]           ; get cell value
+        movdqu xmm0,[rsi]         ; get {cell} x 16
+; get 4 x {neighbour} x 16
+        movdqu xmm3,[rsi-1]       ; left
+        movdqu xmm4,[rsi+1]       ; right
+        movdqu xmm5,[rsi+r8]      ; down
+        movdqu xmm6,[rsi+r11]     ; up
+ ; find next-state values
+        movdqu xmm8,xmm0          ; stash originals
+        paddb xmm0,xmm1           ; find {cell + 1} x 16
+        movdqa xmm9,xmm0
+        pcmpeqb xmm0,xmm2         ; ff for equal, 00 otherwise
+        pandn xmm0,xmm9           ; reset overflowed cell values to 0
+; now we have xmm0 = {cell + 1 (mod states)} x 16
+; detect matching values, leaving xmm4-7 with ff for each position with
+; a match and 00 elsewhere.
+        pcmpeqb xmm3,xmm0
+        pcmpeqb xmm4,xmm0
+        pcmpeqb xmm5,xmm0
+        pcmpeqb xmm6,xmm0
+; combine the results, leaving xmm3 with ff for each position with a match
+; in any neighbour and 00 elsewhere.
+        por xmm3,xmm4
+        por xmm5,xmm6
+        por xmm3,xmm5
+; store {cell+1 (mod states)} x 16 but only where matches were found
+        pmovmskb rax,xmm3
+        maskmovdqu xmm0,xmm3
+        pxor xmm3,xmm7
+        maskmovdqu xmm8,xmm3
+; count how many changes we made
+        popcnt rax,rax
+        add rcx,rax
+        add rsi,16
+        add rdi,16
+        dec rdx
+        jnz mainloop
+; do the last r10 bytes individually
+trailer:
+        cmp r10,0
+        je last
+        add rcx,r10
+byte_loop:
+        mov al,[rsi]           ; get cell value
         mov bl,al              ; stash it for later
         inc al
         cmp al,r9b
-        jb main_nomod
+        jb byte_nomod
         xor rax,rax            ; wrapped around
-main_nomod:
-        cmp al,[rcx-1]         ; left
-        je main_store
-        cmp al,[rcx+1]         ; right
-        je main_store
-        cmp al,[rcx+r8]        ; down
-        je main_store
-        cmp al,[rcx+r11]       ; up
-        je main_store
+byte_nomod:
+        cmp al,[rsi-1]         ; left
+        je byte_store
+        cmp al,[rsi+1]         ; right
+        je byte_store
+        cmp al,[rsi+r8]        ; down
+        je byte_store
+        cmp al,[rsi+r11]       ; up
+        je byte_store
         mov al,bl              ; retrieve original
-        dec rsi
-main_store:
-        mov [rdx],al
-        inc rcx
-        inc rdx
+        dec rcx
+byte_store:
+        mov [rdi],al
+        inc rsi
+        inc rdi
         dec r10
-        jnz mainloop
-; last cell
-        mov al,[rcx]
+        jnz byte_loop
+; last cell is special-cased
+last:
+        mov al,[rsi]
         mov bl,al
         inc al
         cmp al,r9b
         jb last_nomod
         xor rax,rax
 last_nomod:
-        cmp al,[rcx-1]         ; left
+        cmp al,[rsi-1]         ; left
         je last_store
-        cmp al,[rcx+r11+1]     ; right (wrapped)
+        cmp al,[rsi+r11+1]     ; right (wrapped)
         je last_store
-        cmp al,[rcx+r8]        ; down
+        cmp al,[rsi+r8]        ; down
         je last_store
-        cmp al,[rcx+r11]       ; up
+        cmp al,[rsi+r11]       ; up
         je last_store
         mov al,bl              ; retrieve original
-        dec rsi
+        dec rcx
 last_store:
-        mov [rdx],al
-        mov rax,rsi
+        mov [rdi],al
+        inc rcx
+        mov rax,rcx
+        movdqa xmm6,[rsp]
+        movdqa xmm7,[rsp+16]
+        movdqa xmm8,[rsp+32]
+        add rsp,16*3
+        pop rbx
+        pop rsi
+        pop rdi
+        ret
+
 ; int cyclic_vn_64_all(byte *from [rcx], byte *to [rdx],
 ;                      int width [r8], int states [r9],
 ;                      int height [stack])
